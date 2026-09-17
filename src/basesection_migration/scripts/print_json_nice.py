502e6c8cb6d343a5deb25b0d803544c68dc4385b
GinzburgLev <ginzburglev95@gmail.com>
2026-09-17 17:56:15 +0200

    temporary file - add json printing script for tests

---
+++ src/basesection_migration/scripts/print_json_nice.py
@@ -0,0 +1,19 @@
+import argparse
+import json
+import pathlib
+from pathlib import Path
+
+TEMP_FOLDER = 'tests/data/ExampleELN_BSv1'
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description='Print a JSON file with indentation.')
+    parser.add_argument('file_name', type=Path)
+    args = parser.parse_args()
+    print(TEMP_FOLDER + '/' + str(args.file_name))
+    data = json.loads(pathlib.Path(TEMP_FOLDER + '/' + str(args.file_name)).read_text())
+    print(json.dumps(data, indent=4, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    main()
