24d6b662a1e346ac951940373215175852361d21
GinzburgLev <ginzburglev95@gmail.com>
2026-09-21 17:29:27 +0200

    add actual transformation function

--- src/basesection_migration/scripts/update_basesections_step_2.py
+++ src/basesection_migration/scripts/update_basesections_step_2.py
@@ -3,9 +3,9 @@ import importlib
 import json
 import pathlib
 
+from nomad.datamodel.metainfo.annotations import Rules
 from nomad.metainfo.util import metainfo_to_json_schema
-
-# from nomad.datamodel.metainfo.annotations import Rules
+from nomad.utils.json_transformer import Transformer
 
 TEMP_FOLDER = 'tests/data/ExampleELN_BSv1_temp'
 BASE_SECTIONS_V1_LIST = [
@@ -147,60 +147,85 @@ def find_mro(section_definition: str) -> list[type] | None:
             return None
 
 
-def apply_single_transformation(input_section, class_name) -> dict:
-    return {}
+def apply_single_transformation(
+    input_section: dict, target_section: dict, class_name: str, transformer: Transformer
+) -> dict:
+    return transformer.transform(
+        source_data=input_section,
+        mapping_name=f'{class_name.rsplit(".", maxsplit=1)[-1]}_transformation',
+        target_data=target_section,
+        inplace=False,
+        array_rules=True,
+        delete_sources=False,
+    )
 
 
-def transform_section(source_section: dict, section_definition: str) -> dict:
+def transform_section(
+    source_section: dict, section_definition: str, transformer: Transformer
+) -> dict:
+    print(f'### transforming section {section_definition}')
     section_mro = find_mro(section_definition=section_definition)
     if not isinstance(section_mro, list):
         print(f'unexpected mro: {section_mro}')
         return source_section
 
-    result_section = source_section
+    result_section = {}
+    flag_no_transformation = True
 
-    for inherited_class in section_mro:
+    for inherited_class in reversed(section_mro):
         if inherited_class.__name__ in BASE_SECTIONS_V1_LIST:
-            result_section = apply_single_transformation(
-                result_section, inherited_class.__name__
+            print(
+                f'###### applying {inherited_class.__name__}'
+                + f' transformation to section {section_definition}'
             )
+            # print(f'&&&&&& before = {json.dumps(source_section, indent=2)}')
+            result_section = apply_single_transformation(
+                source_section, result_section, inherited_class.__name__, transformer
+            )
+            flag_no_transformation = False
+            # print(f'&&&&&& after = {json.dumps(result_section, indent=2)}')
+
+    if flag_no_transformation:
+        result_section = source_section
+
     return result_section
 
 
 if __name__ == '__main__':
-    # rules_json_paths = [f'rules_{section}.json' for section in BASE_SECTIONS_V1_LIST]
     base_sections_v1_list_classes = import_classes_from_basesections_list()
 
-    # for section in BASE_SECTIONS_V1_LIST:
-    #     rule_path = f'src/basesection_migration/scripts/'
-    #  + 'transformation_rules/rules_{section}.json'
-    #     rules_json = json.loads(pathlib.Path(rule_path).read_text())
-    #     rules = {f'{section}_transformation': Rules(rules_json)}
+    rules = {}
+    for section in BASE_SECTIONS_V1_LIST:
+        rule_path = (
+            'src/basesection_migration/scripts/'
+            + f'transformation_rules/rules_{section}.json'
+        )
+        rules_json = json.loads(pathlib.Path(rule_path).read_text())
+        rules[f'{section}_transformation'] = Rules(**rules_json)
 
-    # transformer = Transformer(rules)
+    # print(f'### rules = {rules}')
+    transformer = Transformer(rules)
 
-    for path in pathlib.Path(TEMP_FOLDER).rglob('*ELNSample.archive.v1.json'):
+    for path in pathlib.Path(TEMP_FOLDER).rglob('*ELNSubstance.archive.v1.json'):
         print(f'Transforming {path}')
-        # TODO: implement the transformation logic here
-        # there are two relevant trees: the MRO of the class (so every transformation in
-        # the inheritance chain is applied) and the subsections tree (the added
-        # subsections can also be inheriting from basesections and therefore need
-        # to be transformed)
         source_data_full = json.loads(path.read_text())
         source_data: dict = source_data_full.get('data', {})
 
         data_m_def: str | None = source_data.get('m_def', None)
 
-        if data_m_def is not None:
-            module_name, separator, class_name = data_m_def.rpartition('.')
-            if separator:
-                try:
-                    data_m_def_class = getattr(
-                        importlib.import_module(module_name), class_name
-                    )
-                except (AttributeError, ImportError, TypeError, ValueError) as e:
-                    data_m_def_class = None
-                    print(f'Failed to import m_def {data_m_def}: {e}')
+        if data_m_def is None:
+            print(f'missing m_def for {path}')
+            continue
+
+        module_name, separator, class_name = data_m_def.rpartition('.')
+        if separator:
+            try:
+                data_m_def_class = getattr(
+                    importlib.import_module(module_name), class_name
+                )
+            except (AttributeError, ImportError, TypeError, ValueError) as e:
+                data_m_def_class = None
+                print(f'Failed to import m_def {data_m_def}: {e}')
 
         if data_m_def_class is not None:
             source_data_schema = metainfo_to_json_schema(
@@ -213,21 +238,41 @@ if __name__ == '__main__':
         list_of_subsections = validate_and_get_subdict_refs(
             source_data, source_data_schema
         )[::-1]
+        list_of_subsections.append(((), data_m_def))
 
         for subsection_information in list_of_subsections:
             print(subsection_information)
             section_path = subsection_information[0]
             section_definition = subsection_information[1]
-            section = source_data
+            old_section = source_data
+            parent_section = source_data_full
+            previous_key = 'data'
             try:
                 for section_path_step in section_path:
-                    section = section[section_path_step]
+                    old_section = old_section[section_path_step]
+                    parent_section = parent_section[previous_key]
+                    previous_key = section_path_step
             except (AttributeError, TypeError, ValueError) as e:
                 print(f'Failed to reach correct subsection, {e}')
                 continue
 
-            if section_definition is not None and isinstance(section, dict):
-                result_data = transform_section(section, section_definition)
+            if section_definition is not None and isinstance(old_section, dict):
+                new_section = transform_section(
+                    old_section, section_definition, transformer
+                )
+                print(f'&&& {new_section}')
+                if isinstance(parent_section, dict):
+                    parent_section.update({previous_key: new_section})
+                elif isinstance(parent_section, list) and isinstance(previous_key, int):
+                    parent_section[previous_key] = new_section
+                else:
+                    raise TypeError
+
+        output_path = path.with_name(
+            path.name.replace('.archive.v1.json', '.archive.v2.json')
+        )
+        with open(output_path, 'w') as f:
+            json.dump(source_data_full, f)
 
         # print(json.dumps(source_data, indent=2))
         # print('###############')
