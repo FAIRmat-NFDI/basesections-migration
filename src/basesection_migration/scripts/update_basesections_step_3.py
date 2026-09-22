316ee88e1a3ff58d13fa06369e6915761a60fa58
GinzburgLev <ginzburglev95@gmail.com>
2026-09-22 14:46:18 +0200

    add step 3 - renaming *.archive.v2.json -> *.archive.json

---
+++ src/basesection_migration/scripts/update_basesections_step_3.py
@@ -0,0 +1,40 @@
+"""rename new entries so that they are picked up by the archive.json parser"""
+
+import pathlib
+
+TEMP_FOLDER = 'tests/data/ExampleELN_BSv1_temp'
+
+
+def rename_v2_archives():
+    """Rename all ``*.archive.v2.json`` files in a folder if corresponding ``v1`` files
+    exists.
+
+    Returns:
+        tuple of list of all renamed paths and list of all ``v2`` paths that were
+        unchanged due to corresponding ``v1`` entries missing.
+    """
+    renamed_paths = []
+    unchanged_paths = []
+    for path in pathlib.Path(TEMP_FOLDER).rglob('*.archive.v2.json'):
+        path_original_entry = path.with_name(
+            path.name.replace('.archive.v2.json', '.archive.v1.json')
+        )
+        if path_original_entry.exists():
+            renamed_path = path.with_name(
+                path.name.replace('.archive.v2.json', '.archive.json')
+            )
+            path.rename(renamed_path)
+            renamed_paths.append(path)
+        else:
+            unchanged_paths.append(path)
+
+    return renamed_paths, unchanged_paths
+
+
+if __name__ == '__main__':
+    renamed_paths, unchanged_paths = rename_v2_archives()
+    if unchanged_paths:
+        print(
+            'Warning: some files has not been renamed '
+            + f'due to missing v1 source files: {unchanged_paths}'
+        )
